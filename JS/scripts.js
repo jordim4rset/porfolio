6bdcd10919fe1b1e2ef072a50b70e4cee66f1931
jordiPortatil <jordimarset22@gmail.com>
2026-01-06 22:22:57 +0100

    acabao

---
+++ JS/scripts.js
@@ -0,0 +1,5 @@
+const btnSubirArriba = document.getElementById('btn-flecha-arriba');
+
+btnSubirArriba.addEventListener('click', () => {
+    btnSubirArriba.style.animation = "latido 0.5s linear infinite";
+});
